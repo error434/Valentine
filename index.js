7509ec349e04eab34dc945edefa90e4d7c1330fd
error434 <65418777+error434@users.noreply.github.com>
2026-02-03 19:00:48 +0530

    Trying shit

---
+++ index.js
@@ -0,0 +1,58 @@
+var noarr=
+[
+    '<i class="bi bi-x-circle"></i>',
+    'Please? <i class="bi bi-x-square"></i>',
+    'Pyaar nahi karti?  <i class="bi bi-emoji-frown"></i>',
+    'Kya hua tera vada?',
+    'Wo kasam wo irada?',
+    'You serious?',
+    '😭',
+    'Momos khilaunga!!',
+    'Yaar 🥺',
+    'Pleaseeeeeeeee',
+    'Bhadwi'
+]
+
+var i = 0;
+var arrlen=noarr.length;
+const nobtn=document.getElementById('nobtn');
+const yesbtn=document.getElementById('yesbtn');
+const gifimage=document.getElementById('gifimage');
+
+function success()
+{
+    const successChange=document.getElementById('successChange');
+    successChange.innerHTML=
+    `<h1 style='text-align:center'>Ok yay !!!</h1>`;
+    gifimage.innerHTML=
+    `<img src='https://tenor.com/nnAg8r4jIfL.gif'>`;
+}
+
+function failPile()
+{
+    if(i<arrlen-1){
+        if(i==4){
+            gifimage.innerHTML=
+            `<img src='https://tenor.com/rZOb1FX42Vv.gif'>`;
+        }
+        i++;
+        showNoBtn();
+        var currentSize=parseFloat(window.getComputedStyle(yesbtn).fontSize);
+        var newSize=currentSize+30;
+        yesbtn.style.fontSize=newSize+'px';
+        }
+    else{
+        i=0;
+        showNoBtn();
+        var currentSize=parseFloat(window.getComputedStyle(yesbtn).fontSize);
+        var newSize=currentSize+4;
+        yesbtn.style.fontSize=newSize+'px';
+    }
+}
+
+function showNoBtn()
+{
+    nobtn.innerHTML=noarr[i];
+}
+
+showNoBtn();
